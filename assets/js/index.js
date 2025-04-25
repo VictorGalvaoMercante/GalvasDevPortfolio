fbcb6bea260c4f98f609f9a46f3f54a5c7f6d177
Galvas <galvaomercante@gmail.com>
2025-04-25 11:02:43 -0300

    Deploy Projeto

---
+++ assets/js/index.js
@@ -0,0 +1,33 @@
+
+const texto = "OLÁ, SOU VICTOR GALVÃO.";
+const elemento = document.querySelector(".digitando");
+
+let i = 0;
+
+function digitar() {
+  if (i < texto.length) {
+    elemento.innerHTML += texto.charAt(i);
+    i++;
+    setTimeout(digitar, 100); // velocidade da digitação
+  }
+}
+
+window.onload = digitar;
+
+
+
+const toggle = document.getElementById('menuToggle');
+const links = document.querySelector('.links');
+const menuLinks = document.querySelectorAll('.links a'); // Pega todos os links dentro do menu
+
+// Ao clicar no toggle, abre ou fecha o menu
+toggle.addEventListener('click', () => {
+  links.classList.toggle('active');
+});
+
+// Adiciona evento de clique em cada link para fechar o menu
+menuLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    links.classList.remove('active');
+  });
+});
